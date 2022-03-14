b3ad9254855a77b024147d4dc7ceac3025286685
lijian7 <lijian7@centaline.com.cn>
2022-03-14 15:08:06 +0800

    first commit

---
+++ app/src/main/java/cc/recent/normal/check_task/CheckTool.java
@@ -0,0 +1,12 @@
+package cc.recent.normal.check_task;
+
+import android.util.Log;
+
+public class CheckTool {
+
+    private static final String TAG = "CheckTool";
+
+    public static void checkTask(){
+        Log.d(TAG, "checkTask: 我是一个检测工具");
+    }
+}
